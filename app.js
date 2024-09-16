f4c7aca1bfb73440915815428300b8ad23c2e8bc
tomasVanek3 <l20220058@ssptaji.cz>
2024-09-16 15:33:30 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch(error => {
+                console.log('Service Worker registration failed:', error);
+            });
+    });
+}
+ 
+function sayHello() {
+    document.getElementById('hello').innerText = 'Hello world';
+}
